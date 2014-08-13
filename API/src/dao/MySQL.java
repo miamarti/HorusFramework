26c6211717009f9e3a044830be766302aec8e629
miamarti <miller.augusto@gmail.com>
2014-08-13 10:37:55 -0300

    organizing packages

--- API/src/dao/MySQL.java
+++ API/src/dao/MySQL.java
@@ -6,7 +6,7 @@ import com.horusframework.annotation.HorusConnectionParameters;
 import com.horusframework.dao.HorusConnection;
 
 public enum MySQL {
-	@HorusConnectionParameters(driverName = "com.mysql.jdbc.Driver", jdbc = "mysql", host = "localhost", port = "3306", database = "test", user = "test", password = "PasswordHornXPTO12345")
+	@HorusConnectionParameters(driverName = "com.mysql.jdbc.Driver", jdbc = "mysql", host = "sjkap356", port = "3306", database = "test", user = "test", password = "E6D0AD7CAA8R")
 	getInstance;
 
 	public Connection getConnection() {
