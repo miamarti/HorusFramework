34b6502597f40d327dde444d309d8f633f8885f7
miamarti <miller.augusto@gmail.com>
2014-08-19 11:05:46 -0300

    Normalizando o MySQL Drive

--- API/src/dao/MySQL.java
+++ API/src/dao/MySQL.java
@@ -3,15 +3,15 @@ package dao;
 import java.sql.Connection;
 
 import com.horusframework.annotation.HorusConnectionParameters;
-import com.horusframework.dao.HorusConnection;
+import com.horusframework.dao.HorusMySQLConnection;
 
 public enum MySQL {
-	@HorusConnectionParameters(driverName = "com.mysql.jdbc.Driver", jdbc = "mysql", host = "sjkap356", port = "3306", database = "test", user = "test", password = "E6D0AD7CAA8R")
+	@HorusConnectionParameters(host = "sjkap356", port = "3306", database = "test", user = "test", password = "E6D0AD7CAA8R")
 	getInstance;
 
 	public Connection getConnection() {
 		try {
-			return HorusConnection.getConnection(this.getDeclaringClass().getField("getInstance").getAnnotation(HorusConnectionParameters.class));
+			return HorusMySQLConnection.getConnection(this.getDeclaringClass().getField("getInstance").getAnnotation(HorusConnectionParameters.class));
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
