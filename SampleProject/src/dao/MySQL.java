89418b0ef090f486aaa90d483fcfe5ddfd95c260
miamarti <miller.augusto@gmail.com>
2014-08-26 17:06:34 -0300

    ...

--- SampleProject/src/dao/MySQL.java
+++ SampleProject/src/dao/MySQL.java
@@ -10,7 +10,7 @@ import org.horusframework.dao.HorusMySQLConnection;
  *
  */
 public enum MySQL {
-	@HorusConnectionParameters(host = "sjkap356", port = "3306", database = "sakila", user = "sakila", password = "PasswordHorn13245")
+	@HorusConnectionParameters(host = "localhost", port = "3306", database = "sakila", user = "sakila", password = "PasswordHorn13245")
 	getInstance;
 
 	public Connection getConnection() {
